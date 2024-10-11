2fbf24a4a8102c96a0e2667eba378e3059cb9796
Kirillsigmaa <ermolaevk998@gmail.com>
2024-10-11 13:25:59 +0300

    arhiriy3

---
+++ app/src/main/java/com/example/arhiriy3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.arhiriy3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void navigateToSearchReception(View view) {
+        Intent insets = new Intent(MainActivity.this, Search_reception.class);
+        startActivity(insets);
+    }
+
+    public void navigateToSearchResults(View view) {
+        Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/arhiriy3/Search_reception.java
@@ -0,0 +1,32 @@
+package com.example.arhiriy3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Search_reception extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.search_reception);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public static final int LAYOUT_SEARCH_RECEPTION = R.layout.search_reception;
+
+    public void navigateToSearchResults(View view) {
+        Intent intent = new Intent(Search_reception.this, SearchActivity.class);
+        startActivity(intent);
+    }
+}
